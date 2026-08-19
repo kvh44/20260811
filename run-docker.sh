8c0e642fd873c0d17548d161303dfe4db21e015b
cheng <bryat.qin@gmail.com>
2026-08-19 09:42:46 -0400

    #000012: Add Readme.md, run-docker.sh, change task definition file

---
+++ run-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+export JAVA_HOME="$(/usr/libexec/java_home -v 26)"
+export PATH="$JAVA_HOME/bin:$PATH"
+
+./mvnw -q -DskipTests package
+
+docker build -t 20260811 .
+docker rm -f 20260811-container >/dev/null 2>&1 || true
+docker run -d --name 20260811-container -p 8001:8001 20260811
+
+echo "Container started. Check with: curl http://localhost:8001/users"
